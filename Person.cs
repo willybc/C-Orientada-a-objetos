5e53305b3b921ec9e2e70a280e9c43851aa49c98
Willy <willybc06@gmail.com>
2020-04-28 20:12:54 -0300

    Clase y objetos

---
+++ Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prueba1
+{
+	public class Person
+	{
+		public string Name;
+		public int Age;
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Prueba1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+
+			Person persona1 = new Person();
+			persona1.Name = "Maria";
+			persona1.Age = 21;
+
+			Person persona2 = new Person();
+			persona2.Name = "Antonio";
+			persona2.Age = 22;
+
+			Console.WriteLine($"persona 1 se llama {persona1.Name} y tiene {persona1.Age} años");
+			Console.WriteLine($"persona 2 se llama {persona2.Name} y tiene {persona2.Age} años");
+
+			Console.ReadKey();
+
+		}
+	}
+}
